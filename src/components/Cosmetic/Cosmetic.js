6cb0a43fe0f95991bc597e6f30261f6e0dc86dff
evo264714 <mh264714@gmail.com>
2023-03-28 23:24:46 +0600

    done

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { addToDb, removeFromDb } from '../../utilities/fakedb';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic
+    const addToCart = (id)=>{
+        addToDb(id)
+    }
+
+    const removeFromCart = id =>{
+        removeFromDb(id);
+    }
+    
     return (
-        <div>
+        <div className='product'>
             <h1>Buy this: {name}</h1>
             <p>Only for: ${price}</p>
             <p><small>It has id: {id}</small></p>
+            
+            <button onClick={() => addToCart(id)}>Add to cart</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
         </div>
     );
 };
